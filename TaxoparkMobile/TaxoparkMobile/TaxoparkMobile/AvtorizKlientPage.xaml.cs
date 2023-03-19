4b7c427eaa570dfcd9183a5fd0bd529c15fec6df
ArtemAfonin28 <artemafonin28@gmail.com>
2023-03-19 22:40:22 +0800

    Beginning of work

---
+++ TaxoparkMobile/TaxoparkMobile/TaxoparkMobile/AvtorizKlientPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TaxoparkMobile
+{
+    public partial class AvtorizKlientPage : ContentPage
+    {
+        public AvtorizKlientPage()
+        {
+            InitializeComponent();
+
+            image1.Source = ImageSource.FromResource("TaxoparkMobile.image.logo.png");
+            imagebutton.Source = ImageSource.FromResource("TaxoparkMobile.image.reg.png");
+
+
+        }
+
+        private async void button1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegKlientPage());
+        }
+    }
+}
---
+++ TaxoparkMobile/TaxoparkMobile/TaxoparkMobile/RegKlientPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace TaxoparkMobile
+{
+    public partial class RegKlientPage : ContentPage
+    {
+        public RegKlientPage()
+        {
+            InitializeComponent();
+
+            image1.Source = ImageSource.FromResource("TaxoparkMobile.image.logo.png");
+            back.Source = ImageSource.FromResource("TaxoparkMobile.image.arrow.png");
+        }
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
